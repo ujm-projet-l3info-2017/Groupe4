84200ff3f43541b0b6f1fe0dadb7ee28b6b89933
MNour7 <mahamadou.nour7@gmail.com>
2017-03-18 16:08:48 +0100

    ajout du controller Equipe

--- StatisfootBundle/Controller/EquipeController.php
+++ StatisfootBundle/Controller/EquipeController.php
@@ -9,5 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 */
 class EquipeController extends Controller
 {
-	
+	public function viewAction($id){
+		$equipe = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_foot')->find($id);
+		return $this->render('ProjetStatisfootBundle:Equipe:view_equipe.html.twig', array('equipe'=>$equipe));
+	}
+
+	public function effectifAction($id){
+		$joueurs = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_foot')->findJoueurs($id);
+		return $this->render('ProjetStatisfootBundle:Equipe:effectif_equipe.html.twig', array('joueurs'=>$joueurs));
+	}
 }
