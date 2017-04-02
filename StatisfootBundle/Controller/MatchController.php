91ee664a86f4dbbde446c7df2c9dd19b448fb699
snianfo <snianfo@gmail.com>
2017-04-02 19:40:30 +0200

    modif matchController

--- StatisfootBundle/Controller/MatchController.php
+++ StatisfootBundle/Controller/MatchController.php
@@ -21,11 +21,9 @@ class MatchController extends Controller
 		
 		//Recuperation desequipes qui ont joues le match
 		$listeMatch =array();
-
 		foreach ($matchs as $ma) {
 			$match_equipe = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_equipe')
 			->findMatchEquipe($ma->getId());
-
 			
 			$ma_eq = array("idMatch"=>$ma->getId(),"heure"=>$ma->getDateMatch(),
 				"NomEq1"=>$match_equipe[0]->getEquipe()->getNom(),"Eq1But"=>$match_equipe[0]->getButMarq(),
@@ -33,29 +31,7 @@ class MatchController extends Controller
 			
 			array_push($listeMatch, $ma_eq);
 		}
-
  		return $this->render('ProjetStatisfootBundle:Match:index.html.twig',array('listeMatch'=>$listeMatch));
-	}
-
-	public function indexDateAction($date){
-		$matchs = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_foot')->findMatchDate($date);
-		
-		//Recuperation des equipes qui ont joués le match
-		$listeMatch =array();
-
-		foreach ($matchs as $ma) {
-			$match_equipe = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_equipe')
-			->findMatchEquipe($ma->getId());
-
-			
-			$ma_eq = array("idMatch"=>$ma->getId(),"heure"=>$ma->getDateMatch(),
-				"NomEq1"=>$match_equipe[0]->getEquipe()->getNom(),"Eq1But"=>$match_equipe[0]->getButMarq(),
-				"NomEq2"=>$match_equipe[1]->getEquipe()->getNom(),"Eq2But"=>$match_equipe[1]->getButMarq());
-			
-			array_push($listeMatch, $ma_eq);
-		}
-
- 		return $this->render('ProjetStatisfootBundle:Match:index_date.html.twig',array('listeMatch'=>$listeMatch));
 	}
 
 	public function match_footAction($id){
@@ -71,9 +47,35 @@ class MatchController extends Controller
 		if($match->getdateMatch() > $date){
 			$infodate = "Match à venir";
 		}
-		//les equipes qui ont joues le match.
+		//Pour chaque equipe on recupere tout qu'il a joue
 
-		//$equipes = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_equipe')->findBy(array('match'=>$id));
-		return $this->render('ProjetStatisfootBundle:Match:match.html.twig', array('match'=>$match, 'infodate'=>$infodate));
+		//recuperation des equipes
+		$adversaires = array(); 
+		$match_equipe = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_equipe')->findMatchEquipe($id);
+		$copies = $match_equipe;
+		foreach ($match_equipe as $equipes) {
+			// les matchs joues par l'equipe
+			$LesMatchDeEquipe = $this->getDoctrine()->getRepository('ProjetStatisfootBundle:match_equipe')->findLesMatchs($equipes->getEquipe()->getId());
+
+			foreach ($LesMatchDeEquipe as $Mequipes) {
+				
+				/*
+				 *Ici pour chaque match on recupere son adversaire
+				 *
+				 */
+				$match_equipe2 = $this->getDoctrine()->getManager()->getRepository('ProjetStatisfootBundle:match_equipe')->findAdversaire($Mequipes->getMatch()->getId(), $equipes->getEquipe()->getId());
+				foreach ($match_equipe2 as $match_eq) {
+				array_push($adversaires,
+				array('IdMatch'=>$match_eq->getMatch()->getId(),
+					'NomEq1'=>$equipes->getEquipe()->getNom(),
+					'Eq1But'=>$match_eq->getButMarq(),
+					'Eq2But'=>$match_eq->getButEnc(),
+					'NomEq2'=>$match_eq->getEquipe()->getNom()));
+				}
+			}
+
+		}
+		
+		return $this->render('ProjetStatisfootBundle:Match:match.html.twig', array('match'=>$match, 'infodate'=>$infodate,'adversaires'=>$adversaires));
 	}
 }
